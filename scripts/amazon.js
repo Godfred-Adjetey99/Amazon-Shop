73f350a8a616aa21f58baeb92cec9420c6f5fc7a
Godfred-Adjetey99 <godfredadjetey99@gmail.com>
2026-04-15 16:56:51 +0000

    added js to the amazon file

---
+++ scripts/amazon.js
@@ -0,0 +1,13 @@
+const products = [
+  {
+    id: 1,
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    price: 10.99,
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    price: 10.99,
+  },
+];
