2c336d8eee2c4121940e5cc2e9373eccd623e625
Godfred-Adjetey99 <godfredadjetey99@gmail.com>
2026-04-23 16:48:58 +0000

    added cartQuantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -70,6 +70,12 @@ document.querySelectorAll(".add-to-cart-button").forEach((button) => {
         quantity: 1,
       });
     }
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    console.log(cartQuantity);
     console.log(cart);
   });
 });
